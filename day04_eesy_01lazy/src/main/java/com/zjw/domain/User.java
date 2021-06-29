347681c313100e3ad82a3875e99cea695bde2d0e
Zhu-junwei <1286630326@qq.com>
2021-06-29 18:16:35 +0800

    简化了一些代码

--- day04_eesy_01lazy/src/main/java/com/zjw/domain/User.java
+++ day04_eesy_01lazy/src/main/java/com/zjw/domain/User.java
@@ -1,9 +1,12 @@
 package com.zjw.domain;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+@Data
 public class User implements Serializable {
     private Integer id;
     private String username;
@@ -13,54 +16,6 @@ public class User implements Serializable {
 
     //建立一对多的关系
     private List<Account> accounts;
-
-    public List<Account> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
 
     @Override
     public String toString() {
